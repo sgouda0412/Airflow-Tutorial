07335722e368ca58f67ce8b389bc22538ec214f9
sgouda0412 <sgouda0412@gmail.com>
2025-08-06 18:48:10 +0530

    Fix:Import issue

--- dags/my_simple_dag.py
+++ dags/my_simple_dag.py
@@ -3,7 +3,7 @@ import datetime as dt
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
-
+import datetime
 
 def greet():
     print('Writing in file')
