70d8a35d50870082b2489102bb729aae763033fa
sgouda0412 <sgouda0412@gmail.com>
2025-08-06 18:53:09 +0530

    Fix:time

--- dags/my_simple_dag.py
+++ dags/my_simple_dag.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 import datetime
-
+import time
 def greet():
     print('Writing in file')
     with open('/Development/airflow_tutorial/greet.txt', 'a+', encoding='utf8') as f:
